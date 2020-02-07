2ffa091bd3a7aaee6257747092979ef74c2c95ba
Trey Suiter <trey.suiter@gmail.com>
2020-02-07 15:14:18 -0600

    post and redirect working for book form

--- libraryapp/views/books/list.py
+++ libraryapp/views/books/list.py
@@ -1,5 +1,5 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from libraryapp.models import Book
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
@@ -32,3 +32,23 @@ def book_list(request):
         }
 
         return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_book
+            (
+                title, author, isbn,
+                year_published, location_id, librarian_id
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['author'],
+                form_data['isbn'], form_data['year_published'],
+                request.user.librarian.id, form_data["location"]))
+
+        return redirect(reverse('libraryapp:books'))
