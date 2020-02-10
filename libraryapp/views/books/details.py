186ba4ed9390a06edff84a62f402ee9a612b4f35
Trey Suiter <trey.suiter@gmail.com>
2020-02-10 11:42:48 -0600

    FEATURE: edit book implmented

--- libraryapp/views/books/details.py
+++ libraryapp/views/books/details.py
@@ -42,6 +42,31 @@ def book_details(request, book_id):
     if request.method == 'POST':
         form_data = request.POST
 
+        # Check if this POST is for editing a book
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "PUT"
+        ):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                UPDATE libraryapp_book
+                SET title = ?,
+                    author = ?,
+                    isbn = ?,
+                    year_published = ?,
+                    location_id = ?
+                WHERE id = ?
+                """,
+                (
+                    form_data['title'], form_data['author'],
+                    form_data['isbn'], form_data['year_published'],
+                    form_data["location"], book_id,
+                ))
+
+            return redirect(reverse('libraryapp:books'))
+
     # Check if this POST is for deleting a book
     #
     # Note: You can use parenthesis to break up complex
